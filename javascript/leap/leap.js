a00930ff151401b66edffdae5c1fe2419777f9a2
Henry Paulino <henrypl360@gmail.com>
2019-02-23 18:53:48 -0500

    leap update

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,5 +1,5 @@
 const isLeapYear = (year) => {
-  return year = (year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0);
+  return (year % 4 == 0) && (year % 100 !== 0) || (year % 400 == 0);
 }
 
 export default isLeapYear;
